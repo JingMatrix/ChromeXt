a419567f9fc7f40c8e022b08189d80890b8952f6
JingMatrix <jingmatrix@gmail.com>
2022-12-11 15:29:01 +0100

    Use createContextForSplit to load split
    Aim: Get tab url and hook page

--- app/src/main/java/org/matrix/chromext/hook/ChromeHook.kt
+++ app/src/main/java/org/matrix/chromext/hook/ChromeHook.kt
@@ -1,13 +1,34 @@
 package org.matrix.chromext.hook
 
 import com.github.kyuubiran.ezxhelper.utils.*
+import com.github.kyuubiran.ezxhelper.utils.Log.logexIfThrow
+import android.content.Context
 
 object ChromeHook : BaseHook() {
-    override fun init() {
-        findMethod("org.chromium.chrome.browser.base.SplitChromeApplication") {
-            name == "attachBaseContext"
-        }.hookBefore {
-            Log.i("Hooked before SplitChromeApplication.attachBaseContext()")
-        }
-    }
+	override fun init() {
+		findMethod("org.chromium.url.GURL") {
+			name == "init"
+		}.hookBefore {
+			// Log.d(it.args[0] as String)
+		}
+
+		findMethod("org.chromium.chrome.browser.base.SplitChromeApplication") {
+			name == "attachBaseContext"
+		}.hookAfter{
+			val ctx: Context = (it.args[0] as Context).createContextForSplit("chrome") 
+				runCatching {
+					findAllMethods(
+							ctx.getClassLoader().loadClass(
+								"org.chromium.chrome.browser.tab.TabImpl")
+							) {
+						paramCount == 1 && isNotAbstract && isPublic && isFinal && isNotNative
+					}.hookBefore {
+						if (it.args[0] != null && it.args[0].javaClass.name == "org.chromium.url.GURL" ) {
+							Log.d("TabImple call GURL ${it.args[0]::class.toString()}")
+						}
+					}
+				}.logexIfThrow("Failed to load TabImple")
+
+		}
+	}   
 }
