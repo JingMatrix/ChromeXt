71d3fb71303c3959e3852a094253381dffa351c1
JingMatrix <jingmatrix@gmail.com>
2022-12-14 15:29:22 +0100

    Store changable smali information
    One can manually configure them in
    /data/data/com.android.chrome/shared_prefs/ChromeXt.xml

--- app/src/main/java/org/matrix/chromext/ChromeXt.kt
+++ app/src/main/java/org/matrix/chromext/ChromeXt.kt
@@ -1,6 +1,7 @@
 package org.matrix.chromext
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.github.kyuubiran.ezxhelper.utils.Log
 import com.github.kyuubiran.ezxhelper.utils.invokeMethod
 import java.lang.reflect.Field
@@ -9,7 +10,7 @@ import org.matrix.chromext.script.youtubeScript
 class ChromeXt(ctx: Context) {
   // These smali code names are possible to change when Chrome updates
   // User should be able to change them by their own if needed
-  // If a field is read-only, i.e., meaning that we are not using it yet
+  // If a field is read-only, i.e., initilized with `val`, meaning that we are not using it yet
 
   // Grep Android.Omnibox.InputToNavigationControllerStart to get loadUrl in
   // org/chromium/content/browser/framehost/NavigationControllerImpl.smali
@@ -50,39 +51,66 @@ class ChromeXt(ctx: Context) {
 
   companion object {
     // These samli code are considered stable for further releases of Chrome
+    // We give two examplar fields.
 
     // The only field with type Ljava/lang/String in
     // org/chromium/url/GURL.smali is for URL
     private const val SPEC_FIELD = "a"
     // ! Note: GURL has limited length:
-    // const size_t kMaxURLChars = 2 * 1024 * 1024 in chromium/src/ur/ url_constants.cc
+    // const size_t kMaxURLChars = 2 * 1024 * 1024; in chromium/src/ur/url_constants.cc
+    // const uint32 kMaxURLChars = 2097152; in chromium/src/url/mojom/url.mojom
 
     // Get TabImpl field in
     // org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroidImpl.smali
     private const val TAB_FIELD = "a"
 
-    // The first filed of org/chromium/content_public/browser/LoadUrlParams should
-    // be the mUrl
-    private const val URL_FIELD = "a"
+    // Fields of org/chromium/content_public/browser/LoadUrlParams
+    // are too many to list here
+    // They are in the same order as the source code
   }
 
   var tabWebContentsDelegateAndroidImpl: Class<*>? = null
-  var interceptNavigationDelegateImpl: Class<*>? = null
-  var navigationControllerImpl: Class<*>? = null
-  var webContentsObserverProxy: Class<*>? = null
-
-  private var gURL: Class<*>? = null
-  private var loadUrlParams: Class<*>? = null
-  private var mUrl: Field? = null
-  private var mSpec: Field? = null
   private var mTab: Field? = null
   private var tabDelegator: Any? = null
 
+  var interceptNavigationDelegateImpl: Class<*>? = null
+
+  var navigationControllerImpl: Class<*>? = null
+  private var navController: Any? = null
+
+  var webContentsObserverProxy: Class<*>? = null
+
+  private val navigationHandle: Class<*>? = null
+
+  private var gURL: Class<*>? = null
+  private var mSpec: Field? = null
+
+  private var loadUrlParams: Class<*>? = null
+  private var mUrl: Field? = null
+  private val mInitiatorOrigin: Field? = null
+  private val mLoadUrlType: Field? = null
+  private val mTransitionType: Field? = null
+  private val mReferrer: Field? = null
+  private val mExtraHeaders: Field? = null
+  private val mNavigationHandleUserDataHost: Field? = null
+  private val mVerbatimHeaders: Field? = null
+  private val mUaOverrideOption: Field? = null
+  private val mPostData: Field? = null
+  private val mBaseUrlForDataUrl: Field? = null
+  private val mVirtualUrlForDataUrl: Field? = null
+  private val mDataUrlAsString: Field? = null
+  private val mCanLoadLocalResources: Field? = null
+  private val mIsRendererInitiated: Field? = null
+  private val mShouldReplaceCurrentEntry: Field? = null
+  private val mIntentReceivedTimestamp: Field? = null
+  private val mInputStartTimestamp: Field? = null
+  private val mHasUserGesture: Field? = null
+  private val mShouldClearHistoryList: Field? = null
+  private val mNavigationUIDataSupplier: Field? = null
+
   init {
-    val sharedPref = ctx.getSharedPreferences("ChromeXt", Context.MODE_PRIVATE)
-    with(sharedPref.edit()) {
-      apply()
-    }
+    val sharedPref: SharedPreferences = ctx.getSharedPreferences("ChromeXt", Context.MODE_PRIVATE)
+    updateSmali(sharedPref)
     gURL = ctx.getClassLoader().loadClass("org.chromium.url.GURL")
     loadUrlParams =
         ctx.getClassLoader().loadClass("org.chromium.content_public.browser.LoadUrlParams")
@@ -97,9 +125,55 @@ class ChromeXt(ctx: Context) {
     webContentsObserverProxy =
         ctx.getClassLoader()
             .loadClass("org.chromium.content.browser.webcontents.WebContentsObserverProxy")
-    mUrl = loadUrlParams!!.getDeclaredField(URL_FIELD)
+    mUrl = loadUrlParams!!.getDeclaredField("a")
     mTab = tabWebContentsDelegateAndroidImpl!!.getDeclaredField(TAB_FIELD)
     mSpec = gURL!!.getDeclaredField(SPEC_FIELD)
+  }
+  private fun updateSmali(sharedPref: SharedPreferences) {
+    if (sharedPref.contains("NAVI_LOAD_URL")) {
+      NAVI_LOAD_URL = sharedPref.getString("NAVI_LOAD_URL", NAVI_LOAD_URL)!!
+    } else {
+      writeSmali(sharedPref)
+      return
+    }
+    if (sharedPref.contains("LOAD_URL")) {
+      LOAD_URL = sharedPref.getString("LOAD_URL", LOAD_URL)!!
+    } else {
+      writeSmali(sharedPref)
+      return
+    }
+  }
+
+  private fun writeSmali(sharedPref: SharedPreferences) {
+    with(sharedPref.edit()) {
+      putString("NAVI_LOAD_URL", NAVI_LOAD_URL)
+      putString("LOAD_URL", LOAD_URL)
+      apply()
+    }
+  }
+
+  private fun loadUrl(url: String) {
+    mTab!!.get(tabDelegator)?.invokeMethod(
+        loadUrlParams!!.getDeclaredConstructor(String::class.java).newInstance(url)) {
+          name == LOAD_URL
+        }
+  }
+
+  private fun evaluateJavaScript(script: String) {
+    val alphabet: List<Char> = ('a'..'z') + ('A'..'Z')
+    val randomString: String = List(14) { alphabet.random() }.joinToString("")
+    loadUrl("javascript: void(globalThis.${randomString} = '')")
+    script.chunked(2090000).forEach {
+      loadUrl("javascript: void(globalThis.${randomString} += '${it.replace("'", "\\'")}')")
+    }
+    loadUrl("javascript: Function(globalThis.${randomString})();")
+  }
+
+  private fun naviUrl(url: String) {
+    navController!!.invokeMethod(
+        loadUrlParams!!.getDeclaredConstructor(String::class.java).newInstance(url)) {
+          name == NAVI_LOAD_URL
+        }
   }
 
   fun parseUrl(packed: Any): String? {
@@ -122,24 +196,29 @@ class ChromeXt(ctx: Context) {
     return false
   }
 
-  fun loadUrl(url: String) {
-    mTab!!.get(tabDelegator)?.invokeMethod(
-        loadUrlParams!!.getDeclaredConstructor(String::class.java).newInstance(url)) {
-          name == LOAD_URL
-        }
-  }
-
   fun updateTabDelegator(delegator: Any): Boolean {
     if (delegator::class.qualifiedName == tabWebContentsDelegateAndroidImpl!!.getName()) {
       if (tabDelegator != delegator) {
         Log.i("tabDelegator updated")
         tabDelegator = delegator
       }
-
       return true
     }
     Log.e(
-        "updateTabDelegator: ${delegator::class.qualifiedName} is not ${loadUrlParams!!.getName()}")
+        "updateTabDelegator: ${delegator::class.qualifiedName} is not ${tabWebContentsDelegateAndroidImpl!!.getName()}")
+    return false
+  }
+
+  fun updateNavController(controller: Any): Boolean {
+    if (controller::class.qualifiedName == navigationControllerImpl!!.getName()) {
+      if (navController != controller) {
+        Log.i("navController updated")
+        navController = controller
+      }
+      return true
+    }
+    Log.e(
+        "updateNavController: ${controller::class.qualifiedName} is not ${navigationControllerImpl!!.getName()}")
     return false
   }
 
@@ -149,7 +228,7 @@ class ChromeXt(ctx: Context) {
 
   fun didStopLoading(url: String) {
     if (url.startsWith("https://m.youtube.com")) {
-      loadUrl("javascript: ${youtubeScript}")
+      evaluateJavaScript(youtubeScript)
     }
   }
 }
--- app/src/main/java/org/matrix/chromext/hook/ChromeHook.kt
+++ app/src/main/java/org/matrix/chromext/hook/ChromeHook.kt
@@ -18,6 +18,7 @@ object ChromeHook : BaseHook() {
           val chromeXt = ChromeXt(ctx)
 
           findMethod(chromeXt.tabWebContentsDelegateAndroidImpl!!) { name == "onUpdateUrl" }
+              // public void onUpdateUrl(GURL url)
               .hookAfter {
                 val url = chromeXt.parseUrl(it.args[0])!!
                 Log.i("onUpdateUrl: ${url}")
@@ -26,41 +27,47 @@ object ChromeHook : BaseHook() {
               }
 
           findMethod(chromeXt.tabWebContentsDelegateAndroidImpl!!) { name == "addMessageToConsole" }
+              // public boolean addMessageToConsole(int level, String message, int lineNumber,
+              // String sourceId)
               .hookAfter {
-                // addMessageToConsole(int level, String message, int lineNumber,
-                // String sourceId)
-
                 // This should be the way to communicate with the front-end of ChromeXt
                 Log.d("[${it.args[0]}] ${it.args[1]} @${it.args[3]}:${it.args[2]}")
               }
 
           findMethod(chromeXt.navigationControllerImpl!!) { name == chromeXt.NAVI_LOAD_URL }
+              // public void loadUrl(LoadUrlParams params)
               .hookBefore {
                 // Currently we only use it to reserve a page
                 val url = chromeXt.parseUrl(it.args[0])!!
+                chromeXt.updateNavController(it.thisObject)
                 if (url.startsWith("chrome://xt/")) {
                   chromeXt.changeUrl(it.args[0], "javascript: 'Page reserved for ChromeXt'")
                 }
               }
 
           findAllMethods(chromeXt.webContentsObserverProxy!!) { name == "didStartLoading" }
+              // public void didStartLoading(GURL url)
               .hookBefore {
                 val url = chromeXt.parseUrl(it.args[0])!!
                 chromeXt.didStartLoading(url)
               }
 
           findAllMethods(chromeXt.webContentsObserverProxy!!) { name == "didStopLoading" }
+              // public void didStopLoading(GURL url, boolean isKnownValid)
               .hookAfter {
                 val url = chromeXt.parseUrl(it.args[0])!!
                 chromeXt.didStopLoading(url)
               }
 
-          // findMethod(chromeXt.interceptNavigationDelegateImpl!!) {
-          //       name == "shouldIgnoreNavigation"
-          //     }
-          //     .hookAfter {
-          //       // Not using it yet, could help AdAway in the future
-          //     }
+          findMethod(chromeXt.interceptNavigationDelegateImpl!!) {
+                name == "shouldIgnoreNavigation"
+              }
+              // public boolean shouldIgnoreNavigation(NavigationHandle navigationHandle, GURL
+              // escapedUrl)
+              .hookAfter {
+                // Not using it yet, could help AdAway in the future
+                // Source code not stable yet, we should postpone relevant implements
+              }
         }
   }
 }
