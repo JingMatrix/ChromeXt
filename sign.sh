8d412c845b45df50e75ecfd87f51a5f91b085765
JingMatrix <jingmatrix@gmail.com>
2022-12-19 15:43:07 +0100

    Sign apk with Github Action

--- sign.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-./gradlew ktfmtFormat && ./gradlew build
-apk-singer -a app/build/outputs/apk/release/app-release-unsigned.apk --ks ~/.ssh/apk-release.keystore --ksPass $(cat ~/.ssh/apk.key) --ksKeyPass $(cat ~/.ssh/apk.key) --ksAlias $USER -o .
-mv ./app-release-aligned-signed.apk /tmp/ChromeXt.apk
-adb install /tmp/ChromeXt.apk
