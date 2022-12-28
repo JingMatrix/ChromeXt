373dc892b0c1bb910293bbaaad8e27545d273172
JingMatrix <jingmatrix@gmail.com>
2022-12-28 12:42:22 +0100

    Use Filter js class for better structure

--- app/src/main/assets/local_eruda.js
+++ app/src/main/assets/local_eruda.js
@@ -2,23 +2,64 @@ if (
   typeof globalThis.eruda != "undefined" &&
   typeof eruda.configured == "undefined"
 ) {
+  class Filter {
+    #filter = [];
+    constructor() {}
+    refresh() {
+      let filter = localStorage.getItem("ChromeXt_filter");
+      filter = JSON.parse(filter);
+      if (Array.isArray(filter)) {
+        this.#filter = filter;
+      } else {
+        localStorage.removeItem("ChromeXt_filter");
+      }
+    }
+    add(rule) {
+      if (typeof rule == "string") {
+        rule = rule.trim();
+        if (rule != "" && !this.#filter.includes(rule)) {
+          this.#filter.push(rule);
+          this.#write();
+        }
+      }
+    }
+    get() {
+      return this.#filter;
+    }
+    remove(rule) {
+      this.#filter = this.#filter.filter((item) => item.trim() !== rule);
+      this.#write();
+    }
+    new() {
+      this.#filter.push("");
+      this.#write();
+    }
+    #write() {
+      if (this.#filter.length > 0) {
+        localStorage.setItem("ChromeXt_filter", JSON.stringify(this.#filter));
+      } else {
+        localStorage.removeItem("ChromeXt_filter");
+      }
+    }
+    save() {
+      this.#filter = [];
+      document
+        .querySelector("#eruda")
+        .shadowRoot.querySelectorAll(".eruda-filter-item")
+        .forEach((it) => this.#filter.push(it.innerText.trim()));
+      this.remove("");
+    }
+  }
+
+  eruda._filter = new Filter();
   class elements extends eruda.Elements {
     constructor() {
       super();
+      eruda._filter.refresh();
       this._deleteNode = () => {
         const node = this._curNode;
         const selector = generateQuerySelector(node);
-        let filter = localStorage.getItem("ChromeXt_filter");
-        if (filter == null) {
-          filter = [selector];
-        } else {
-          filter = JSON.parse(filter);
-          if (!filter.includes(selector)) {
-            filter.push(selector);
-          }
-        }
-        localStorage.setItem("ChromeXt_filter", JSON.stringify(filter));
-
+        eruda._filter.add(selector);
         if (node.parentNode) {
           node.parentNode.removeChild(node);
         }
@@ -29,48 +70,23 @@ if (
   class resources extends eruda.Resources {
     _bindEvent() {
       super._bindEvent();
-      const self = this;
       const $el = this._$el;
+      const self = this;
       const container = this._container;
       $el
         .on("click", ".eruda-delete-filter", function (e) {
-          const key = e.curTarget.dataset.key;
-          let filter = JSON.parse(localStorage.getItem("ChromeXt_filter"));
-          filter = filter.filter((item) => item !== key);
-          if (filter.length > 0) {
-            localStorage.setItem("ChromeXt_filter", JSON.stringify(filter));
-          } else {
-            localStorage.removeItem("ChromeXt_filter");
-          }
+          const rule = e.curTarget.dataset.key;
+          eruda._filter.remove(rule);
           self.refreshLocalStorage()._render();
         })
         .on("click", ".eruda-add-filter", () => {
-          let filter = localStorage.getItem("ChromeXt_filter");
-          if (filter != null) {
-            filter = JSON.parse(filter);
-            filter.push("");
-            localStorage.setItem("ChromeXt_filter", JSON.stringify(filter));
-          } else {
-            localStorage.setItem("ChromeXt_filter", '[""]');
-          }
+          eruda._filter.new();
           self.refreshLocalStorage()._render();
         })
         .on("click", ".eruda-save-filter", () => {
-          container.notify("Filter Saved");
-          let filter = [];
-          document
-            .querySelector("#eruda")
-            .shadowRoot.querySelectorAll(".eruda-filter-item")
-            .forEach((it) => {
-              const rule = it.innerText.trim();
-              if (rule != "") {
-                filter.push(rule);
-              }
-            });
-          if (filter.length > 0) {
-            localStorage.setItem("ChromeXt_filter", JSON.stringify(filter));
-          }
+          eruda._filter.save();
           self.refreshLocalStorage()._render();
+          container.notify("Filter Saved");
         });
     }
     _renderHtml(html) {
@@ -82,13 +98,11 @@ if (
       };
       let filterHtml = "<tr><td>Empty</td></tr>";
 
-      let filter = localStorage.getItem("ChromeXt_filter");
-      if (filter != null) {
-        filter = JSON.parse(filter);
-        if (Array.isArray(filter)) {
-          filterHtml = filter
-            .map(
-              (key) => `<tr>
+      let filter = eruda._filter.get();
+      if (filter.length > 0) {
+        filterHtml = filter
+          .map(
+            (key) => `<tr>
           <td contentEditable="true" class="${c("filter-item")}">${key}</td>
           <td class="${c("control")}">
             <span class="${c(
@@ -96,21 +110,18 @@ if (
             )}" data-key="${key}" data-type="local"></span>
           </td>
         </tr>`
-            )
-            .join("");
-        } else {
-          localStorage.removeItem("ChromeXt_filter");
-        }
+          )
+          .join("");
       }
 
       const ChromeXtFilterHtml = `<div class="${c("section ChromeXt-filter")}">
 		<h2 class="${c("title")}">ChromeXt Cosmetic Filters
 			<div class="${c(
         "btn save-filter"
-      )}" style="padding-left: 5px"><span>💾</span></div>
+      )}" style="font-size:12px;"><span>💾</span></div>
 			<div class="${c(
         "btn add-filter"
-      )}" style="padding-right: 5px"><span>➕</span></div>
+      )}" style="font-size:12px;"><span>➕</span></div>
 		</h2>
 		<div class="${c("content")}">
         <table>
@@ -133,14 +144,13 @@ if (
 function generateQuerySelector(el) {
   if (el.tagName.toLowerCase() == "html") return "html";
   let str = el.tagName.toLowerCase();
-  str += el.id != "" ? "#" + el.id : "";
-  if (el.className != "") {
-    let classes = el.className.split(/\s/);
+  if (el.id != "") {
+    str += "#" + el.id;
+  } else if (el.className != "") {
+    let classes = el.className.split(/\s/).filter((rule) => rule.trim() != "");
     str += "." + classes.join(".");
-  }
-  if (str == el.tagName.toLowerCase()) {
-    return generateQuerySelector(el.parentNode) + " > " + str;
   } else {
-    return str;
+    return generateQuerySelector(el.parentNode) + " > " + str;
   }
+  return str;
 }
