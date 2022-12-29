9ffe3ddd177ecda146293037a1e985fa68ba4e06
JingMatrix <jingmatrix@gmail.com>
2022-12-29 14:09:29 +0100

    Disable hooking menu if not dev Mode
    This allow user to locate the problems by themselves.

--- app/src/main/java/org/matrix/chromext/hook/Menu.kt
+++ app/src/main/java/org/matrix/chromext/hook/Menu.kt
@@ -20,72 +20,70 @@ object MenuHook : BaseHook() {
 
     val proxy = MenuProxy(ctx, split)
 
+    // Page menu only appear after restarting chrome
+    if (proxy.isDeveloper) {
+      findMethod(proxy.chromeTabbedActivity) { name == proxy.MENU_KEYBOARD_ACTION }
+          // public boolean onMenuOrKeyboardAction(int id, boolean fromMenu)
+          .hookAfter {
+            val id = it.args[0] as Int
+            val name = ctx.getResources().getResourceName(id)
+            if (name == "org.matrix.chromext:id/developer_tools_id") {
+              UserScriptHook.proxy!!.openDevTools(ctx)
+            }
+          }
+
+      findMethod(proxy.appMenuPropertiesDelegateImpl) {
+            name == proxy.UPDATE_REQUEST_DESKTOP_SITE_MENU_ITEM
+          }
+          // protected void updateRequestDesktopSiteMenuItem(Menu menu, @Nullable Tab currentTab,
+          //         boolean canShowRequestDesktopSite, boolean isChromeScheme)
+          .hookBefore {
+            val menu = it.args[0] as Menu
+            MenuInflater(ctx).inflate(R.menu.main_menu, menu)
+
+            val mItems = menu::class.java.getDeclaredField("mItems")
+            mItems.setAccessible(true)
+
+            @Suppress("UNCHECKED_CAST") val items = mItems.get(menu) as ArrayList<MenuItem>
+
+            val devMenuItem: MenuItem = items.removeLast()
+            devMenuItem.setVisible(!(it.args[3] as Boolean) && (it.args[2] as Boolean))
+            // The index 13 is just chosen by tests, to make sure that it appears before the share
+            // menu
+            items.add(13, devMenuItem)
+            mItems.setAccessible(false)
+          }
+    }
+
+    // Preference menu doesn't work with non-split version
     if (split) {
       findMethod(proxy.chromeTabbedActivity) { name == "onStart" }
           .hookAfter {
             val activity = it.thisObject as Activity
             GestureConflict.hookActivity(activity)
           }
-    }
 
-    findMethod(proxy.chromeTabbedActivity) { name == proxy.MENU_KEYBOARD_ACTION }
-        // public boolean onMenuOrKeyboardAction(int id, boolean fromMenu)
-        .hookAfter {
-          val id = it.args[0] as Int
-          val name = ctx.getResources().getResourceName(id)
-          if (name == "org.matrix.chromext:id/developer_tools_id") {
-            UserScriptHook.proxy!!.openDevTools(ctx)
-          }
-        }
-
-    findMethod(proxy.appMenuPropertiesDelegateImpl) {
-          name == proxy.UPDATE_REQUEST_DESKTOP_SITE_MENU_ITEM
-        }
-        // protected void updateRequestDesktopSiteMenuItem(Menu menu, @Nullable Tab currentTab,
-        //         boolean canShowRequestDesktopSite, boolean isChromeScheme)
-        .hookBefore {
-          val menu = it.args[0] as Menu
-          MenuInflater(ctx).inflate(R.menu.main_menu, menu)
-
-          val mItems = menu::class.java.getDeclaredField("mItems")
-          mItems.setAccessible(true)
-
-          @Suppress("UNCHECKED_CAST") val items = mItems.get(menu) as ArrayList<MenuItem>
-
-          val devMenuItem: MenuItem = items.removeLast()
-          devMenuItem.setVisible(
-              !(it.args[3] as Boolean) && (it.args[2] as Boolean) && proxy.isDeveloper)
-          // The index 13 is just chosen by tests, to make sure that it appears before the share
-          // menu
-          items.add(13, devMenuItem)
-          mItems.setAccessible(false)
-        }
-
-    if (!split) {
-      // Couldn't hook Preference Menu for unknown reason. might be the resource problem
-      return
-    }
-
-    findMethod(proxy.preferenceFragmentCompat) { name == proxy.ADD_PREFERENCES_FROM_RESOURCE }
-        // public void addPreferencesFromResource(Int preferencesResId)
-        .hookBefore {
-          if (it.thisObject::class.qualifiedName == proxy.developerSettings.name) {
-            it.args[0] = R.xml.developer_preferences
-          }
-        }
-
-    findMethod(proxy.preferenceFragmentCompat) { name == proxy.FIND_PREFERENCE }
-        // public @Nullable T <T extends Preference> findPreference(@NonNull CharSequence key)
-        .hookAfter {
-          if (it.thisObject::class.qualifiedName == proxy.developerSettings.name &&
-              (it.args[0] as String) == "beta_stable_hint") {
-
-            val refThis = it
-            arrayOf("eruda", "exit").forEach {
-              proxy.setClickListenerAndSummary(
-                  (refThis.method as Method).invoke(refThis.thisObject, it)!!, ctx, it)
+      findMethod(proxy.preferenceFragmentCompat) { name == proxy.ADD_PREFERENCES_FROM_RESOURCE }
+          // public void addPreferencesFromResource(Int preferencesResId)
+          .hookBefore {
+            if (it.thisObject::class.qualifiedName == proxy.developerSettings.name) {
+              it.args[0] = R.xml.developer_preferences
             }
           }
-        }
+
+      findMethod(proxy.preferenceFragmentCompat) { name == proxy.FIND_PREFERENCE }
+          // public @Nullable T <T extends Preference> findPreference(@NonNull CharSequence key)
+          .hookAfter {
+            if (it.thisObject::class.qualifiedName == proxy.developerSettings.name &&
+                (it.args[0] as String) == "beta_stable_hint") {
+
+              val refThis = it
+              arrayOf("eruda", "exit").forEach {
+                proxy.setClickListenerAndSummary(
+                    (refThis.method as Method).invoke(refThis.thisObject, it)!!, ctx, it)
+              }
+            }
+          }
+    }
   }
 }
