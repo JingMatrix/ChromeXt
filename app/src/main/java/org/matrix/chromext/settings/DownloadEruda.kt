cd497876a6f72b4cbab21300618eab8513edef1d
JingMatrix <jingmatrix@gmail.com>
2023-01-06 19:46:57 +0100

    Separate out a Download object
    Prepare for the @resource API

--- app/src/main/java/org/matrix/chromext/settings/DownloadEruda.kt
+++ app/src/main/java/org/matrix/chromext/settings/DownloadEruda.kt
@@ -1,60 +1,30 @@
 package org.matrix.chromext.settings
 
-import android.app.DownloadManager
-import android.app.DownloadManager.Request
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.Uri
-import android.os.Environment
 import android.view.View
 import android.view.View.OnClickListener
-import java.io.FileReader
 import org.matrix.chromext.Chrome
 import org.matrix.chromext.proxy.MenuProxy
+import org.matrix.chromext.utils.Download
 import org.matrix.chromext.utils.Log
 
+const val ERUD_URL = "https://cdn.jsdelivr.net/npm/eruda@latest/eruda.min.js"
+
 object DownloadEruda : OnClickListener {
-
-  var id: Long? = null
-
   override fun onClick(v: View) {
-    Chrome.getContext()
-        .registerReceiver(
-            writeSharedPreference, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-
-    val request = Request(Uri.parse("https://cdn.jsdelivr.net/npm/eruda@latest/eruda.min.js"))
-    request.setDescription("Console for mobile browsers")
-    request.setTitle("Eruda.js")
-    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Eruda.js")
-    val downloadManager =
-        Chrome.getContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-    id = downloadManager.enqueue(request)
-  }
-
-  object writeSharedPreference : BroadcastReceiver() {
-    override fun onReceive(ctx: Context, intent: Intent) {
-      if (intent.getAction() == DownloadManager.ACTION_DOWNLOAD_COMPLETE) {
-        val downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0)
-        if (downloadId == id) {
-          val downloadManager = ctx.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-          val fd = downloadManager.openDownloadedFile(downloadId).getFileDescriptor()
-          val eruda = FileReader(fd).use { it.readText() }
-          val old_version = MenuProxy.getErudaVersion()
-          val sharedPref = ctx.getSharedPreferences("Eruda", Context.MODE_PRIVATE)
-          with(sharedPref!!.edit()) {
-            putString("eruda", eruda)
-            apply()
-          }
-          val new_version = MenuProxy.getErudaVersion()
-          if (old_version != new_version) {
-            Log.toast(ctx, "Updated to eruda v" + MenuProxy.getErudaVersion()!!)
-          } else {
-            Log.toast(ctx, "Eruda is already the lastest")
-          }
-          downloadManager.remove(downloadId)
-        }
+    Download.start(ERUD_URL, "Download/Eruda.js") {
+      val old_version = MenuProxy.getErudaVersion()
+      val ctx = Chrome.getContext()
+      val sharedPref = ctx.getSharedPreferences("Eruda", Context.MODE_PRIVATE)
+      with(sharedPref!!.edit()) {
+        putString("eruda", it)
+        apply()
+      }
+      val new_version = MenuProxy.getErudaVersion()
+      if (old_version != new_version) {
+        Log.toast(ctx, "Updated to eruda v" + MenuProxy.getErudaVersion()!!)
+      } else {
+        Log.toast(ctx, "Eruda is already the lastest")
       }
     }
   }
---
+++ app/src/main/java/org/matrix/chromext/utils/Download.kt
@@ -0,0 +1,65 @@
+package org.matrix.chromext.utils
+
+import android.app.DownloadManager
+import android.app.DownloadManager.Request
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import java.io.File
+import java.io.FileReader
+import org.matrix.chromext.Chrome
+
+object Download {
+
+  private var id: Long? = null
+
+  fun start(
+      url: String,
+      path: String,
+      keep: Boolean = false,
+      callback: ((content: String) -> Unit)? = null
+  ) {
+    runCatching {
+          val request = Request(Uri.parse(url))
+
+          val file = File(Chrome.getContext().getExternalFilesDir(null), path)
+          if (file.exists()) {
+            file.delete()
+          }
+
+          request.setDestinationUri(Uri.fromFile(file))
+
+          val receiver = createCallBack(keep, callback)
+          Chrome.getContext()
+              .registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+          val downloadManager =
+              Chrome.getContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+          id = downloadManager.enqueue(request)
+        }
+        .onFailure { Log.ex(it) }
+  }
+
+  private fun createCallBack(
+      keep: Boolean,
+      callback: ((content: String) -> Unit)?
+  ): BroadcastReceiver {
+    return object : BroadcastReceiver() {
+      override fun onReceive(ctx: Context, intent: Intent) {
+        if (intent.getAction() == DownloadManager.ACTION_DOWNLOAD_COMPLETE) {
+          val downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0)
+          if (downloadId == id) {
+            val downloadManager = ctx.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val fd = downloadManager.openDownloadedFile(downloadId).getFileDescriptor()
+            callback?.invoke(FileReader(fd).use { it.readText() })
+            if (!keep) {
+              downloadManager.remove(downloadId)
+            }
+          }
+        }
+      }
+    }
+  }
+}
