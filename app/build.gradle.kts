10223b8e76689e82cb6fec99835b86f650880d7e
JingMatrix <jingmatrix@gmail.com>
2023-02-16 14:27:30 +0100

    Release Chrome v2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "org.matrix.chromext"
     minSdk = 26
     targetSdk = 33
-    versionCode = 2
-    versionName = "2.0.0"
+    versionCode = 3
+    versionName = "2.1.0"
   }
 
   buildFeatures { buildConfig = true }
