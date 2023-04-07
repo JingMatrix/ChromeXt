120bfa02ab19d39a39eddd9d7d0460f85691b950
JingMatrix <jingmatrix@gmail.com>
2023-04-07 19:12:06 +0200

    Fix GestureNav Hook

--- app/src/main/java/org/matrix/chromext/hook/GestureNav.kt
+++ app/src/main/java/org/matrix/chromext/hook/GestureNav.kt
@@ -2,12 +2,13 @@ package org.matrix.chromext.hook
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Insets
 import android.graphics.Rect
 import android.os.Build
+import android.view.WindowInsets
 import java.lang.ref.WeakReference
 import kotlin.math.roundToInt
 import org.matrix.chromext.Chrome
-import org.matrix.chromext.proxy.GestureNavProxy
 import org.matrix.chromext.utils.findMethod
 import org.matrix.chromext.utils.hookAfter
 import org.matrix.chromext.utils.hookBefore
@@ -17,38 +18,43 @@ object GestureNavHook : BaseHook() {
   override fun init() {
 
     var activity: WeakReference<Activity>? = null
-    val proxy = GestureNavProxy()
 
-    findMethod(proxy.historyNavigationCoordinator) {
-          getParameterCount() == 0 && getReturnType() == Boolean::class.java
+    findMethod(Chrome.load("org.chromium.chrome.browser.ChromeTabbedActivity"), true) {
+          name == "onStart"
         }
-        // private boolean isFeatureEnabled()
+        .hookAfter { activity = WeakReference(it.thisObject as Activity) }
+
+    findMethod(WindowInsets::class.java) { name == "getSystemGestureInsets" }
         .hookBefore {
           val sharedPref =
               Chrome.getContext()
                   .getSharedPreferences(
                       Chrome.getContext().getPackageName() + "_preferences", Context.MODE_PRIVATE)
           if (sharedPref.getBoolean("gesture_mod", true)) {
-            fixConflict(activity!!.get()!!)
-            it.result = true
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+              it.result = Insets.of(0, 0, 0, 0)
+            }
+            fixConflict(activity!!.get()!!, true)
+          } else {
+            fixConflict(activity!!.get()!!, false)
           }
         }
-
-    findMethod(proxy.chromeTabbedActivity, true) { name == "onStart" }
-        .hookAfter { activity = WeakReference(it.thisObject as Activity) }
   }
 
-  fun fixConflict(activity: Activity) {
+  fun fixConflict(activity: Activity, excludeSystemGesture: Boolean) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
       val decoView = activity.getWindow().getDecorView()
       val width = decoView.getWidth()
       val height = decoView.getHeight()
       val excludeHeight: Int =
           (activity.getResources().getDisplayMetrics().density * 100).roundToInt()
-      // Log.d("Called setSystemGestureExclusionRects with size ${width} x ${excludeHeight * 2}")
-      decoView.setSystemGestureExclusionRects(
-          // public Rect (int left, int top, int right, int bottom)
-          listOf(Rect(width / 2, height / 2 - excludeHeight, width, height / 2 + excludeHeight)))
+      if (excludeSystemGesture) {
+        decoView.setSystemGestureExclusionRects(
+            // public Rect (int left, int top, int right, int bottom)
+            listOf(Rect(width / 2, height / 2 - excludeHeight, width, height / 2 + excludeHeight)))
+      } else {
+        decoView.setSystemGestureExclusionRects(listOf(Rect(0, 0, 0, 0)))
+      }
     }
   }
 }
