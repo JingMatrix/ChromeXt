8a4ddedb6797d26d602f57189957c4d1073d4561
JingMatrix <jingmatrix@gmail.com>
2023-04-24 17:34:26 +0200

    addProvider method might throw error
    For unknown reason, the module_path doesn't exist

--- app/src/main/java/org/matrix/chromext/ResourceMerge.kt
+++ app/src/main/java/org/matrix/chromext/ResourceMerge.kt
@@ -1,12 +1,6 @@
 package org.matrix.chromext
 
 import android.content.Context
-import android.content.res.loader.ResourcesLoader
-import android.content.res.loader.ResourcesProvider
-import android.os.Build
-import android.os.ParcelFileDescriptor
-import java.io.File
-import java.net.URI
 import org.matrix.chromext.utils.invokeMethod
 
 object ResourceMerge {
@@ -26,16 +20,6 @@ object ResourceMerge {
       }
     }
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      val resLoader = ResourcesLoader()
-      resLoader.addProvider(
-          ResourcesProvider.loadFromApk(
-              ParcelFileDescriptor.open(
-                  File(URI.create("file://" + module_path!!)),
-                  ParcelFileDescriptor.MODE_READ_ONLY)))
-      ctx.getResources().addLoaders(resLoader)
-    } else {
-      ctx.assets.invokeMethod(module_path!!) { name == "addAssetPath" }
-    }
+    ctx.assets.invokeMethod(module_path!!) { name == "addAssetPath" }
   }
 }
