b07fdcf9f71e528b98760d481d747f5f5f0d0d38
JingMatrix <jingmatrix@gmail.com>
2023-04-26 22:53:43 +0200

    Update kotlin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import com.ncorti.ktfmt.gradle.tasks.KtfmtFormatTask
 
 plugins {
-  id("com.android.application") version "8.0.0-alpha11" apply false
-  id("com.android.library") version "8.0.0-alpha11" apply false
-  id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+  id("com.android.application") version "8.1.0-alpha11" apply false
+  id("com.android.library") version "8.1.0-alpha11" apply false
+  id("org.jetbrains.kotlin.android") version "1.8.21" apply false
   id("com.ncorti.ktfmt.gradle") version "0.12.0"
 }
 
