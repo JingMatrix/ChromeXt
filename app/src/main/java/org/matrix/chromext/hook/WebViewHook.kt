e04f94a2b8106fef81c301060442a34388cac5bd
JingMatrix <jingmatrix@gmail.com>
2023-05-20 02:56:53 +0200

    Hook loadUrl of WebView

--- app/src/main/java/org/matrix/chromext/hook/WebViewHook.kt
+++ app/src/main/java/org/matrix/chromext/hook/WebViewHook.kt
@@ -4,7 +4,6 @@ import android.webkit.ConsoleMessage
 import android.webkit.WebView
 import org.json.JSONObject
 import org.matrix.chromext.Chrome
-import org.matrix.chromext.DEV_FRONT_END
 import org.matrix.chromext.script.ScriptDbManager
 import org.matrix.chromext.script.encodeScript
 import org.matrix.chromext.script.urlMatch
@@ -24,58 +23,8 @@ object WebWiewHook : BaseHook() {
     ResourceMerge.enrich(ctx)
     val promptInstallUserScript =
         ctx.assets.open("editor.js").bufferedReader().use { it.readText() }
-    val customizeDevTool = ctx.assets.open("devtools.js").bufferedReader().use { it.readText() }
     val cosmeticFilter =
         ctx.assets.open("cosmetic-filter.js").bufferedReader().use { it.readText() }
-
-    findMethod(ViewClient!!, true) { name == "onPageStarted" }
-        // public void onPageStarted (WebView view, String url, Bitmap favicon)
-        .hookAfter {
-          val view = it.args[0] as WebView
-          val url = it.args[1] as String
-          view.evaluateJavascript("globalThis.ChromeXt=console.debug.bind(console);", null)
-          if (url.endsWith(".user.js")) {
-            view.evaluateJavascript(promptInstallUserScript, null)
-          } else if (url.startsWith(DEV_FRONT_END)) {
-            view.evaluateJavascript(customizeDevTool, null)
-          } else if (!url.endsWith("/ChromeXt/")) {
-            ScriptDbManager.scripts.forEach loop@{
-              val script = it
-              script.exclude.forEach {
-                if (urlMatch(it, url, true)) {
-                  return@loop
-                }
-              }
-              script.match.forEach {
-                if (urlMatch(it, url, false)) {
-                  val code = encodeScript(script)
-                  if (code != null) {
-                    Log.i("${script.id} injected")
-                    view.evaluateJavascript(code, null)
-                    Log.d("Run script: ${script.code.replace("\\s+".toRegex(), " ")}")
-                  }
-                  return@loop
-                }
-              }
-            }
-
-            val protocol = url.split("://")
-            if (protocol.size > 1 && arrayOf("https", "http", "file").contains(protocol.first())) {
-              val origin = protocol.first() + "://" + protocol.elementAt(1).split("/").first()
-              if (ScriptDbManager.cosmeticFilters.contains(origin)) {
-                view.evaluateJavascript(
-                    "globalThis.ChromeXt_filter=`${ScriptDbManager.cosmeticFilters.get(origin)}`;${cosmeticFilter}",
-                    null)
-                Log.d("Cosmetic filters applied to ${origin}")
-              }
-              if (ScriptDbManager.userAgents.contains(origin)) {
-                view.evaluateJavascript(
-                    "Object.defineProperties(window.navigator,{userAgent:{value:'${ScriptDbManager.userAgents.get(origin)}'}});",
-                    null)
-              }
-            }
-          }
-        }
 
     findMethod(ChromeClient!!, true) {
           name == "onConsoleMessage" &&
@@ -91,12 +40,7 @@ object WebWiewHook : BaseHook() {
                   val data = JSONObject(text)
                   val action = data.getString("action")
                   val payload = data.getString("payload")
-                  runCatching {
-                        val callback = ScriptDbManager.on(action, payload)
-                        if (callback != null) {
-                          // proxy.evaluateJavascript(callback)
-                        }
-                      }
+                  runCatching { ScriptDbManager.on(action, payload) }
                       .onFailure { Log.w("Failed with ${action}: ${payload}") }
                 }
                 .onFailure { Log.d("Ignore console.debug: " + text) }
@@ -106,5 +50,59 @@ object WebWiewHook : BaseHook() {
                     ": [${consoleMessage.sourceId()}@${consoleMessage.lineNumber()}] ${consoleMessage.message()}")
           }
         }
+
+    fun onUpdateUrl(url: String, view: WebView) {
+      val enableJS = view.settings.javaScriptEnabled
+      view.settings.javaScriptEnabled = true
+      view.evaluateJavascript("globalThis.ChromeXt=console.debug.bind(console);", null)
+      if (url.endsWith(".user.js")) {
+        view.evaluateJavascript(promptInstallUserScript, null)
+      } else if (!url.endsWith("/ChromeXt/")) {
+        val protocol = url.split("://")
+        if (protocol.size > 1 && arrayOf("https", "http", "file").contains(protocol.first())) {
+          ScriptDbManager.scripts.forEach loop@{
+            val script = it
+            script.exclude.forEach {
+              if (urlMatch(it, url, true)) {
+                return@loop
+              }
+            }
+            script.match.forEach {
+              if (urlMatch(it, url, false)) {
+                val code = encodeScript(script)
+                if (code != null) {
+                  Log.i("${script.id} injected")
+                  view.evaluateJavascript(code, null)
+                  Log.d("Run script: ${script.code.replace("\\s+".toRegex(), " ")}")
+                }
+                return@loop
+              }
+            }
+          }
+
+          val origin = protocol.first() + "://" + protocol[1].split("/").first()
+          if (ScriptDbManager.cosmeticFilters.contains(origin)) {
+            view.evaluateJavascript(
+                "globalThis.ChromeXt_filter=`${ScriptDbManager.cosmeticFilters.get(origin)}`;${cosmeticFilter}",
+                null)
+            Log.d("Cosmetic filters applied to ${origin}")
+          }
+          if (ScriptDbManager.userAgents.contains(origin)) {
+            view.evaluateJavascript(
+                "Object.defineProperties(window.navigator,{userAgent:{value:'${ScriptDbManager.userAgents.get(origin)}'}});",
+                null)
+          }
+        }
+        view.settings.javaScriptEnabled = enableJS
+      }
+    }
+
+    findMethod(WebView::class.java) { name == "loadUrl" }
+        // public void loadUrl (String url)
+        .hookAfter { onUpdateUrl(it.args[0] as String, it.thisObject as WebView) }
+
+    findMethod(ViewClient!!, true) { name == "onPageStarted" }
+        // public void onPageStarted (WebView view, String url, Bitmap favicon)
+        .hookAfter { onUpdateUrl(it.args[1] as String, it.args[0] as WebView) }
   }
 }
