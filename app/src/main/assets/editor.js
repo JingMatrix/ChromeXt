7b84d7c7aca67968dde070997355fca75780ca64
JingMatrix <jingmatrix@gmail.com>
2023-06-30 00:16:28 +0200

    Only allow editing UTF-8 encoded scripts

--- app/src/main/assets/editor.js
+++ app/src/main/assets/editor.js
@@ -1,44 +1,48 @@
-let old_script = "";
-
-function installScript(install = false) {
-  let script = document.querySelector("body > pre").innerText;
-  if (script != old_script) {
-    old_script = script;
-    if (!install) {
-      install = confirm("Allow ChromeXt to install / update this userscript?");
+async function installScript(force = false) {
+  if (!force) {
+    force = confirm("Confirm ChromeXt to install this UserScript?");
+  }
+  if (force) {
+    let script;
+    if (document.characterSet != "UTF-8") {
+      const response = await fetch(window.location.href);
+      script = await response.text();
+    } else {
+      script = document.body.innerText;
     }
-    if (install) {
-      ChromeXt(JSON.stringify({ action: "installScript", payload: script }));
-    }
+    ChromeXt(JSON.stringify({ action: "installScript", payload: script }));
   }
 }
 
 function editor() {
-  const js = document.createElement("script");
+  const separator = "==/UserScript==\n";
+  const code = document.querySelector("body > pre");
+  const script = code.innerHTML.split(separator);
+
+  const scriptMeta = document.createElement("pre");
   const meta = document.createElement("meta");
   const style = document.createElement("style");
-  js.setAttribute("type", "module");
-  js.textContent =
-    "import {highlightElement} from 'https://unpkg.com/@speed-highlight/core/dist/index.js';highlightElement(document.querySelector('body > pre'), 'js', {hideLineNumbers: true});";
+  const js = document.createElement("script");
+  scriptMeta.innerHTML = script.shift() + separator;
+  scriptMeta.id = "meta";
+  code.innerHTML = script.join(separator);
+  code.id = "code";
+  code.removeAttribute("style");
   style.setAttribute("type", "text/css");
   meta.setAttribute("name", "viewport");
   meta.setAttribute(
     "content",
     "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
   );
-  style.textContent = `
-	@import url('https://unpkg.com/@speed-highlight/core/dist/themes/default.css');
-	html {
-		overflow-x: hidden;
-	}
-	body {
-		margin: 0;
-	}
+  style.textContent = `@import url('https://unpkg.com/@speed-highlight/core/dist/themes/default.css');
+	html {overflow-x: hidden}
+	pre {overflow-x: scroll}
+	body { margin: 0}
 	[class*="shj-lang-"] {
 		border-radius: 0;
 		color: #abb2bf;
 		background: #161b22;
-		font: 1.2em monospace;
+		font: 1em monospace;
 		padding: 8px 5px;
 		margin: 0;
 		width: 100vw;
@@ -59,12 +63,19 @@ function editor() {
 	.shj-syn-num,
 	.shj-syn-bool {color: #d19a66}
 	.shj-syn-str,
-	.shj-syn-func {color: #61afef}
-	`;
+	.shj-syn-func {color: #61afef}`;
+  js.setAttribute("type", "module");
+  js.textContent =
+    "import { highlightElement } from 'https://unpkg.com/@speed-highlight/core/dist/index.js'; highlightElement(document.querySelector('#code'), 'js', { hideLineNumbers: true });";
+
+  document.body.prepend(scriptMeta);
   document.head.appendChild(meta);
   document.head.appendChild(style);
-  document.body.appendChild(js);
-  document.querySelector("body > pre").setAttribute("contenteditable", true);
+  document.head.appendChild(js);
+  if (document.characterSet == "UTF-8") {
+    scriptMeta.setAttribute("contenteditable", true);
+    code.setAttribute("contenteditable", true);
+  }
   setTimeout(installScript, 500);
 }
 
