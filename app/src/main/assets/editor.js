4019b10d52bfdbc555b4cfa718ba41f55a827dea
JingMatrix <jingmatrix@gmail.com>
2023-08-11 02:01:02 +0200

    Improve editor rendering

--- app/src/main/assets/editor.js
+++ app/src/main/assets/editor.js
@@ -8,39 +8,39 @@ async function installScript(force = false) {
   }
 }
 
-renderEditor = () => {
+function renderEditor() {
   if (window.editorReady) return;
   window.editorReady = true;
 
+  const code = document.querySelector("body > pre");
+
+  const separator = "==/UserScript==\n";
+  const script = code.innerHTML.split(separator);
+  if (separator.length == 1) return;
+  const scriptMeta = document.createElement("pre");
+  scriptMeta.innerHTML = (script.shift() + separator).replace(
+    "GM.ChromeXt",
+    "<em>GM.ChromeXt</em>"
+  );
+  scriptMeta.id = "meta";
+  code.innerHTML = script.join(separator);
+  code.id = "code";
+  code.removeAttribute("style");
+  document.body.prepend(scriptMeta);
+
+  scriptMeta.setAttribute("contenteditable", true);
+  code.setAttribute("contenteditable", true);
+  setTimeout(installScript);
   import("https://unpkg.com/@speed-highlight/core/dist/index.js").then(
     (imports) => {
-      const code = document.querySelector("body > pre");
-
-      const separator = "==/UserScript==\n";
-      const script = code.innerHTML.split(separator);
-      if (separator.length == 1) return;
-      const scriptMeta = document.createElement("pre");
-      scriptMeta.innerHTML = (script.shift() + separator).replace(
-        "GM.ChromeXt",
-        "<em>GM.ChromeXt</em>"
-      );
-      scriptMeta.id = "meta";
-      code.innerHTML = script.join(separator);
-      code.id = "code";
-      code.removeAttribute("style");
-      document.body.prepend(scriptMeta);
-
-      scriptMeta.setAttribute("contenteditable", true);
-      code.setAttribute("contenteditable", true);
       imports.highlightElement(document.querySelector("#code"), "js", {
         hideLineNumbers: true,
       });
-      setTimeout(installScript);
     }
   );
-};
+}
 
-function prepareDOM() {
+async function prepareDOM() {
   const meta = document.createElement("meta");
   const style = document.createElement("style");
 
@@ -59,19 +59,18 @@ function prepareDOM() {
     const code = document.querySelector("body > pre");
     const text = code.textContent;
     if (!/^[\p{ASCII}]*$/u.test(text)) {
-      import("https://cdn.jsdelivr.net/npm/windows-1252@latest/+esm").then(
-        (windows1252) => {
-          const bytes_16 = windows1252.encode(code.textContent, {
-            mode: "replacement",
-          });
-          const bytes = new Uint8Array(bytes_16);
-          const utf8 = new TextDecoder();
-          code.textContent = utf8.decode(bytes);
-        }
+      const windows1252 = await import(
+        "https://cdn.jsdelivr.net/npm/windows-1252@latest/+esm"
       );
+      const bytes_16 = windows1252.encode(code.textContent, {
+        mode: "replacement",
+      });
+      const bytes = new Uint8Array(bytes_16);
+      const utf8 = new TextDecoder();
+      code.textContent = utf8.decode(bytes);
     }
   }
-  setTimeout(renderEditor);
+  renderEditor();
 }
 
 if (document.readyState == "complete") {
