4d011d26e80296d8ea6585f6634d807ddde307d3
JingMatrix <jingmatrix@gmail.com>
2023-08-17 20:16:29 +0200

    Use local windows1252 encoding function

--- app/src/main/assets/editor.js
+++ app/src/main/assets/editor.js
@@ -83,7 +83,7 @@ function fixDialog() {
   installScript();
 }
 
-async function prepareDOM() {
+function prepareDOM() {
   const meta = document.createElement("meta");
   const style = document.createElement("style");
 
@@ -102,11 +102,11 @@ async function prepareDOM() {
   document.head.appendChild(meta);
   document.head.appendChild(style);
 
-  await fixEncoding();
+  fixEncoding();
   renderEditor();
 }
 
-async function fixEncoding() {
+function fixEncoding() {
   const gb18030 = {
     // Code taken from https://github.com/EtherDream/str2gbk/blob/main/index.js
     table: null,
@@ -178,15 +178,185 @@ async function fixEncoding() {
     },
   };
 
+  const windows1252 = {
+    // Code taken from https://github.com/mathiasbynens/windows-1252
+    INDEX_BY_CODE_POINT: new Map([
+      [129, 1],
+      [141, 13],
+      [143, 15],
+      [144, 16],
+      [157, 29],
+      [160, 32],
+      [161, 33],
+      [162, 34],
+      [163, 35],
+      [164, 36],
+      [165, 37],
+      [166, 38],
+      [167, 39],
+      [168, 40],
+      [169, 41],
+      [170, 42],
+      [171, 43],
+      [172, 44],
+      [173, 45],
+      [174, 46],
+      [175, 47],
+      [176, 48],
+      [177, 49],
+      [178, 50],
+      [179, 51],
+      [180, 52],
+      [181, 53],
+      [182, 54],
+      [183, 55],
+      [184, 56],
+      [185, 57],
+      [186, 58],
+      [187, 59],
+      [188, 60],
+      [189, 61],
+      [190, 62],
+      [191, 63],
+      [192, 64],
+      [193, 65],
+      [194, 66],
+      [195, 67],
+      [196, 68],
+      [197, 69],
+      [198, 70],
+      [199, 71],
+      [200, 72],
+      [201, 73],
+      [202, 74],
+      [203, 75],
+      [204, 76],
+      [205, 77],
+      [206, 78],
+      [207, 79],
+      [208, 80],
+      [209, 81],
+      [210, 82],
+      [211, 83],
+      [212, 84],
+      [213, 85],
+      [214, 86],
+      [215, 87],
+      [216, 88],
+      [217, 89],
+      [218, 90],
+      [219, 91],
+      [220, 92],
+      [221, 93],
+      [222, 94],
+      [223, 95],
+      [224, 96],
+      [225, 97],
+      [226, 98],
+      [227, 99],
+      [228, 100],
+      [229, 101],
+      [230, 102],
+      [231, 103],
+      [232, 104],
+      [233, 105],
+      [234, 106],
+      [235, 107],
+      [236, 108],
+      [237, 109],
+      [238, 110],
+      [239, 111],
+      [240, 112],
+      [241, 113],
+      [242, 114],
+      [243, 115],
+      [244, 116],
+      [245, 117],
+      [246, 118],
+      [247, 119],
+      [248, 120],
+      [249, 121],
+      [250, 122],
+      [251, 123],
+      [252, 124],
+      [253, 125],
+      [254, 126],
+      [255, 127],
+      [338, 12],
+      [339, 28],
+      [352, 10],
+      [353, 26],
+      [376, 31],
+      [381, 14],
+      [382, 30],
+      [402, 3],
+      [710, 8],
+      [732, 24],
+      [8211, 22],
+      [8212, 23],
+      [8216, 17],
+      [8217, 18],
+      [8218, 2],
+      [8220, 19],
+      [8221, 20],
+      [8222, 4],
+      [8224, 6],
+      [8225, 7],
+      [8226, 21],
+      [8230, 5],
+      [8240, 9],
+      [8249, 11],
+      [8250, 27],
+      [8364, 0],
+      [8482, 25],
+    ]),
+    encodingError(mode) {
+      if (mode === "replacement") {
+        return 0xfffd;
+      }
+      // Else, `mode == 'fatal'`.
+      throw new Error();
+    },
+    encode(input, options) {
+      let mode;
+      if (options && options.mode) {
+        mode = options.mode.toLowerCase();
+      }
+      // Support `fatal` (default) and `replacement` error modes.
+      if (mode !== "fatal" && mode !== "replacement") {
+        mode = "fatal";
+      }
+      const length = input.length;
+      const result = new Uint16Array(length);
+      for (let index = 0; index < length; index++) {
+        const codePoint = input.charCodeAt(index);
+        // “If `code point` is an ASCII code point, return a byte whose
+        // value is `code point`.”
+        if (0x00 <= codePoint && codePoint <= 0x7f) {
+          result[index] = codePoint;
+          continue;
+        }
+        // “Let `pointer` be the index pointer for `code point` in index
+        // single-byte.”
+        if (this.INDEX_BY_CODE_POINT.has(codePoint)) {
+          const pointer = this.INDEX_BY_CODE_POINT.get(codePoint);
+          // “Return a byte whose value is `pointer + 0x80`.”
+          result[index] = pointer + 0x80;
+        } else {
+          // “If `pointer` is `null`, return `error` with `code point`.”
+          result[index] = this.encodingError(mode);
+        }
+      }
+      return result;
+    },
+  };
+
   const code = document.querySelector("body > pre");
   const text = code.textContent;
 
   if (document.characterSet != "UTF-8" && !/^[\p{ASCII}]*$/u.test(text)) {
     let converter = () => new Uint8Array();
     if (document.characterSet == "windows-1252") {
-      const windows1252 = await import(
-        "https://cdn.jsdelivr.net/npm/windows-1252@latest/+esm"
-      );
       converter = function (text) {
         const bytes_16 = windows1252.encode(text, {
           mode: "replacement",
