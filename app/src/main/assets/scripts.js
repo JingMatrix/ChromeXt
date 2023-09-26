bb8c19e0f4a50b366135652c05a4bb5765388cb2
JingMatrix <jingmatrix@gmail.com>
2023-09-26 06:11:03 +0200

    Skip sync of SyncArray during initialization

--- app/src/main/assets/scripts.js
+++ app/src/main/assets/scripts.js
@@ -51,7 +51,7 @@ if (typeof Symbol.ChromeXt == "undefined") {
           }
         }
         const result = super[method](...args);
-        this.sync();
+        ChromeXt.isLocked() && this.sync();
         return result;
       };
 
