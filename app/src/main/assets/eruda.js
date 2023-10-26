64fe512a55ae431dfdce1099794c7122163e4659
JingMatrix <jingmatrix@gmail.com>
2023-10-26 09:30:07 +0200

    Sanitizer API deprecated after v118
    See https://developer.chrome.com/blog/sanitizer-api-deprecation/ for
    details

--- app/src/main/assets/eruda.js
+++ app/src/main/assets/eruda.js
@@ -80,7 +80,6 @@ eruda._initDevTools = new Proxy(eruda._initDevTools, {
       }
     };
     this.typesHooked = true;
-    if (typeof Element.prototype.setHTML != "function") return;
     const _enable = eruda.chobitsu.domain("Overlay").enable;
     eruda.chobitsu.domain("Overlay").enable = function () {
       if (_enable.enabled) return;
@@ -93,7 +92,13 @@ eruda._initDevTools = new Proxy(eruda._initDevTools, {
       const tooltip = overlay.querySelector("div.luna-dom-highlighter > div");
       Object.defineProperty(tooltip, "innerHTML", {
         set(value) {
-          this.setHTML(value);
+          if (this.innerHTML == value) return true;
+          try {
+            this.setHTML(value);
+          } catch {
+            this.textContent = value;
+          }
+          return true;
         },
       });
     };
