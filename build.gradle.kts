6c24b489d4ab6dc69891aadf0818f68bcdc27c23
JingMatrix <jingmatrix@gmail.com>
2024-11-22 23:06:04 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import com.ncorti.ktfmt.gradle.tasks.KtfmtFormatTask
 
 plugins {
-  id("com.android.application") version "8.5.1" apply false
-  id("org.jetbrains.kotlin.android") version "2.0.10" apply false
-  id("com.ncorti.ktfmt.gradle") version "0.19.0"
+  id("com.android.application") version "8.7.2" apply false
+  id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+  id("com.ncorti.ktfmt.gradle") version "0.21.0"
 }
 
 tasks.register<KtfmtFormatTask>("format") {
