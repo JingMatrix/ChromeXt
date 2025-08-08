503a154d69230867bae70736d3e631096477ed22
JingMatrix <jingmatrix@gmail.com>
2025-08-08 19:02:15 +0200

    Adapting to new page menu design of Chrome
    According to this issue, Chromium now chooses to creat menus manually.
    
    It becomes harder for us to adapt to this new MVC (Model-View-Controller) design.

--- app/src/main/java/org/matrix/chromext/hook/PageMenu.kt
+++ app/src/main/java/org/matrix/chromext/hook/PageMenu.kt
@@ -128,6 +128,7 @@ object PageMenuHook : BaseHook() {
               findMenuHook!!.unhook()
               val appMenuPropertiesDelegateImpl = it.result::class.java.superclass as Class<*>
               // Can be found by searching `Android.PrepareMenu`
+              val tabbedAppMenuPropertiesDelegate = it.result::class.java
 
               val parameters = appMenuPropertiesDelegateImpl.declaredConstructors[0].parameterTypes
 
@@ -144,7 +145,7 @@ object PageMenuHook : BaseHook() {
                 // and it customize the menu by onFooterViewInflated() function in
                 // https://github.com/brave/brave-core/blob/master/android/java/
                 // org/chromium/chrome/browser/appmenu/BraveTabbedAppMenuPropertiesDelegate.java
-                findMethod(it.result::class.java, true) {
+                findMethod(tabbedAppMenuPropertiesDelegate, true) {
                       parameterTypes.size == 2 && getParameterTypes()[1] == View::class.java
                     }
                     // public void onFooterViewInflated(AppMenuHandler appMenuHandler, View view)
@@ -161,94 +162,116 @@ object PageMenuHook : BaseHook() {
                     }
               }
 
-              findMethod(appMenuPropertiesDelegateImpl, true) {
+              val prepareMenu =
+                  findMethodOrNull(appMenuPropertiesDelegateImpl, true) {
                     parameterTypes.size == 2 &&
                         parameterTypes.first() == Menu::class.java &&
                         returnType == Void.TYPE &&
                         !Modifier.isStatic(modifiers) &&
                         !Modifier.isAbstract(modifiers)
                   }
-                  // public void prepareMenu(Menu menu, AppMenuHandler handler)
-                  .hookAfter inflate@{
-                    val tabProvider = mActivityTabProvider.get(it.thisObject)!!
-                    Chrome.updateTab(tabProvider.invokeMethod { name == "get" })
-                    val ctx = mContext.get(it.thisObject) as Context
-                    Resource.enrich(ctx)
-
-                    val menu = it.args[0] as Menu
-                    val url = getUrl()
-
-                    val iconRowMenu = menu.getItem(0)
-                    if (iconRowMenu.hasSubMenu() && !Chrome.isBrave) {
-                      val infoMenu = iconRowMenu.getSubMenu()!!.getItem(3)
-                      infoMenu.setIcon(R.drawable.ic_book)
-                      infoMenu.setEnabled(true)
-                      val mId = infoMenu::class.java.getDeclaredField("mId")
-                      mId.setAccessible(true)
-                      mId.set(infoMenu, readerMode.ID)
-                      mId.setAccessible(false)
+              // public void prepareMenu(Menu menu, AppMenuHandler handler)
+              if (prepareMenu == null) {
+                val maybeAddDividerLine =
+                    findMethod(tabbedAppMenuPropertiesDelegate) {
+                      parameterTypes.size == 2 &&
+                          parameterTypes[1] == Int::class.java &&
+                          returnType == Void.TYPE &&
+                          !Modifier.isAbstract(modifiers)
                     }
-
-                    val mItems = menu::class.java.getDeclaredField("mItems")
-                    mItems.setAccessible(true)
-
-                    @Suppress("UNCHECKED_CAST") val items = mItems.get(menu) as ArrayList<MenuItem>
-
-                    val skip = items.filter { it.isVisible() }.size <= 10 || isChromeScheme(url)
-                    // Inflate only for the main_menu, which has more than visible 10 items at least
-
-                    if (skip && !isUserScript(url)) return@inflate
-                    MenuInflater(ctx).inflate(R.menu.main_menu, menu)
-
-                    // Show items with indices in main_menu.xml
-                    val toShow = mutableListOf<Int>(1)
-
-                    if (isDevToolsFrontEnd(url)) {
-                      toShow.clear()
+                val listAdapter_ModelList = maybeAddDividerLine.parameterTypes.first()
+                val mItems =
+                    findField(listAdapter_ModelList, true) { type == ArrayList::class.java }
+                findMethod(tabbedAppMenuPropertiesDelegate) {
+                      parameterTypes.size == 0 && returnType == listAdapter_ModelList
                     }
-
-                    if (isUserScript(url)) {
-                      toShow.clear()
-                      toShow.add(2)
-                      if (skip) {
-                        // Show this menu for local preview pages (Custom Tab) of UserScripts
-                        items.find { it.itemId == R.id.install_script_id }?.setVisible(true)
-                        mItems.setAccessible(false)
-                        return@inflate
-                      }
+                    .hookAfter {
+						 @Suppress("UNCHECKED_CAST")
+                      val items = mItems.get(it.result) as ArrayList<Any>
+                      items.forEach { Log.d("${it}") }
                     }
+              } else {
+                prepareMenu.hookAfter inflate@{
+                  val tabProvider = mActivityTabProvider.get(it.thisObject)!!
+                  Chrome.updateTab(tabProvider.invokeMethod { name == "get" })
+                  val ctx = mContext.get(it.thisObject) as Context
+                  Resource.enrich(ctx)
 
-                    if (isChromeXtFrontEnd(url)) {
-                      toShow.clear()
-                      toShow.addAll(listOf(3, 4))
-                    }
+                  val menu = it.args[0] as Menu
+                  val url = getUrl()
 
-                    if (!Chrome.isVivaldi &&
-                        ctx.resources.configuration.smallestScreenWidthDp >=
-                            DisplayMetrics.DENSITY_XXHIGH &&
-                        toShow.size == 1 &&
-                        toShow.first() == 1) {
-                      iconRowMenu.setVisible(true)
-                    }
-
-                    val position =
-                        items
-                            .withIndex()
-                            .filter {
-                              ctx.resources
-                                  .getResourceName(it.value.getItemId())
-                                  .endsWith("id/divider_line_id")
-                            }
-                            .map { it.index }[1]
-
-                    toShow.forEach {
-                      val newMenuItem: MenuItem = items[items.size - it]
-                      newMenuItem.setVisible(true)
-                      items.add(position + 1, newMenuItem)
-                    }
-                    for (i in 0..3) items.removeLast()
-                    mItems.setAccessible(false)
+                  val iconRowMenu = menu.getItem(0)
+                  if (iconRowMenu.hasSubMenu() && !Chrome.isBrave) {
+                    val infoMenu = iconRowMenu.getSubMenu()!!.getItem(3)
+                    infoMenu.setIcon(R.drawable.ic_book)
+                    infoMenu.setEnabled(true)
+                    val mId = infoMenu::class.java.getDeclaredField("mId")
+                    mId.setAccessible(true)
+                    mId.set(infoMenu, readerMode.ID)
+                    mId.setAccessible(false)
                   }
+
+                  val mItems = menu::class.java.getDeclaredField("mItems")
+                  mItems.setAccessible(true)
+
+                  @Suppress("UNCHECKED_CAST") val items = mItems.get(menu) as ArrayList<MenuItem>
+
+                  val skip = items.filter { it.isVisible() }.size <= 10 || isChromeScheme(url)
+                  // Inflate only for the main_menu, which has more than visible 10 items at least
+
+                  if (skip && !isUserScript(url)) return@inflate
+                  MenuInflater(ctx).inflate(R.menu.main_menu, menu)
+
+                  // Show items with indices in main_menu.xml
+                  val toShow = mutableListOf<Int>(1)
+
+                  if (isDevToolsFrontEnd(url)) {
+                    toShow.clear()
+                  }
+
+                  if (isUserScript(url)) {
+                    toShow.clear()
+                    toShow.add(2)
+                    if (skip) {
+                      // Show this menu for local preview pages (Custom Tab) of UserScripts
+                      items.find { it.itemId == R.id.install_script_id }?.setVisible(true)
+                      mItems.setAccessible(false)
+                      return@inflate
+                    }
+                  }
+
+                  if (isChromeXtFrontEnd(url)) {
+                    toShow.clear()
+                    toShow.addAll(listOf(3, 4))
+                  }
+
+                  if (!Chrome.isVivaldi &&
+                      ctx.resources.configuration.smallestScreenWidthDp >=
+                          DisplayMetrics.DENSITY_XXHIGH &&
+                      toShow.size == 1 &&
+                      toShow.first() == 1) {
+                    iconRowMenu.setVisible(true)
+                  }
+
+                  val position =
+                      items
+                          .withIndex()
+                          .filter {
+                            ctx.resources
+                                .getResourceName(it.value.getItemId())
+                                .endsWith("id/divider_line_id")
+                          }
+                          .map { it.index }[1]
+
+                  toShow.forEach {
+                    val newMenuItem: MenuItem = items[items.size - it]
+                    newMenuItem.setVisible(true)
+                    items.add(position + 1, newMenuItem)
+                  }
+                  for (i in 0..3) items.removeLast()
+                  mItems.setAccessible(false)
+                }
+              }
             }
     isInit = true
   }
